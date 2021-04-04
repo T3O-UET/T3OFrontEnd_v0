818e91f511d09adac7f452b6fdc0243de9be611c
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2021-04-04 10:47:26 +0700

    update

--- T3O/Navigators/CartNavigator.js
+++ T3O/Navigators/CartNavigator.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { createStackNavigator } from "@react-navigation/stack"
 
 import Cart from '../Screens/Cart/Cart';
-// import CheckoutNavigator from './CheckoutNavigator';
+import CheckoutNavigator from './CheckoutNavigator';
 
 const Stack = createStackNavigator();
 
