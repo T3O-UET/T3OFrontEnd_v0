65cebdbbc09e40b76a7606d4e69b3426b56d51af
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2021-04-08 17:06:07 +0700

    update

--- T3O/Screens/Cart/Checkout/Checkout.js
+++ T3O/Screens/Cart/Checkout/Checkout.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 import { Text, View } from 'react-native'
+import {Item, Picker } from 'native-base'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import FormContainer from '../../../Shared/Form/FormContainer'
+import Input from '../../../Shared/Form/Input'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+import { connect } from 'react-redux'
 
 const Checkout = () => {
+
+    const [ orderItems, setOrderItems ] = useState();
+    const [ address, setAddress ] = useState();
+    const [ address2, setAddress2 ] = useState();
+    const [ city, setCity ] = useState();
+    const [ phone, setPhone ] = useState();
+
+    useEffect(() => {
+        setOrderItems(props.cartItems)
+
+        return () => {
+            setOrderItems();
+        }
+    }, [])
+
     return (
-        <View>
-            <Text>Checkout Screen</Text>
-        </View>
+        <KeyboardAwareScrollView
+            viewIsInsideTabBar={true}
+            extraHight={200}
+        >
+
+        </KeyboardAwareScrollView>
     )
 }
 
