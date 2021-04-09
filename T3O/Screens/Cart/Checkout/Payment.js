2eb055de09fa1a91d021e086dd6c4de7260c7f4a
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2021-04-09 17:25:40 +0700

    update

--- T3O/Screens/Cart/Checkout/Payment.js
+++ T3O/Screens/Cart/Checkout/Payment.js
@@ -1,13 +1,76 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { View, Button } from 'react-native'
+import {
+    Container,
+    Header,
+    Content,
+    ListItem,
+    Text,
+    Radio,
+    Right,
+    Left,
+    Picker,
+    Icon,
+    Body,
+    Title
+} from 'native-base';
 
-const Payment = () => {
-    return (
-        <View>
-            <Text>Payment Screen</Text>
-        </View>
+const methods = [
+    { name: 'Thanh toán khi nhận hàng.', value: 1 },
+    { name: 'Momo', value: 2 },
+    { name: 'Tài khoản ngân hàng', value: 3}
+]
+
+const Payment = (props) => {
+
+    const order = props.route.params;
+
+    const [selected, setSelected] = useState();
+    const [card, setCard] = useState();
+    return(
+       <Container>
+           <Header>
+               <Body>
+                   <Title>Chọn hình thức thanh toán</Title>
+               </Body>
+           </Header>
+           <Content>
+               {methods.map((item, index) => {
+                   return (
+                       <ListItem key={item.name} onPress={() => setSelected(item.value)}>
+                           <Left>
+                            <Text>{item.name}</Text>
+                           </Left>
+                           <Right>
+                               <Radio selected={selected == item.value}/>
+                           </Right>
+                       </ListItem>
+                   )
+               })}
+               {selected == 3 ? (
+                <Picker
+                    selectedValue={card}
+                    headerStyle={{ backgroundColor: 'orange' }}
+                    headerBackButtonTextStyle={{ color: '#fff' }}
+                    headerTitleStyle={{ color: '#fff' }}
+                    style={{ height: 50, width: 150 }}
+                    onValueChange={(x) => setCard(x)}
+                  >
+                    <View style={{ paddingLeft: 100}}>  
+                    <Picker.Item label="BIDV" value="bidv" />
+                    <Picker.Item label="Techcombank" value="techcombank" />      
+                    <Picker.Item label="Vietcombank" value="vietcombank" /> 
+                    </View>
+                   </Picker>
+               ) : null }
+               <View style={{ marginTop: 60, alignSelf: 'center' }}>
+                       <Button 
+                       title={"Confirm"} 
+                       onPress={() => props.navigation.navigate("Confirm", { order })}/>
+               </View>
+           </Content>
+       </Container>
     )
 }
-
 
 export default Payment;
