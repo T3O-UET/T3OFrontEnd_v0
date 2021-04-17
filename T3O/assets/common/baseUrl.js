e6d71569611c43530842bbd3ccdd6915710ca76a
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2021-04-17 11:36:46 +0700

    update

--- T3O/assets/common/baseUrl.js
+++ T3O/assets/common/baseUrl.js
@@ -1,7 +1,13 @@
 import {Platform} from 'react-native'
+
+//Home
 // let baseURL = 'http://192.168.1.227:3000/api/v1';
-let baseURL = 'http://10.90.136.151:3000/api/v1';
-// let baseURL = 'https://t3o-store-server.herokuapp.com/api/v1'
+
+// 2.4 GHz G2
+// let baseURL = 'http://10.90.136.151:3000/api/v1';
+
+//heroku Server
+let baseURL = 'https://t3ostore-server.herokuapp.com/api/v1'
 
 // {Platform.OS == 'android'
 // ? baseURL = 'http://10.0.2.2:3000/api/v1/'
